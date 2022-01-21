e5fbc6c91c36980a8655090d02da80a3b2cc5948
AndreScheir <scheir5@hotmail.se>
2022-01-22 00:00:07 +0100

    Hello mr singleton

---
+++ singleton.py
@@ -0,0 +1,14 @@
+class Singleton:
+    def __init__(self, usrCls):
+        self.usrCls = usrCls
+    def get_instance(self):
+        try:
+            return self.instance
+        except:
+            self.instance = self.usrCls()
+            return self.instance
+    def __call__(self):
+        raise TypeError('Get instance from get_instance')
+
+    def __instancecheck__(self, instance) -> bool:
+        return isinstance(instance, self.usrCls)
